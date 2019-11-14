43fad69760d9c041ce1a869595dbe530b0b6902c
Matthew Jones <matts3371@gmail.com>
2019-11-14 23:09:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age| 
-    if kids_name[age] < 12
+    if birthday_kids[kids_name] < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end 
   end 
